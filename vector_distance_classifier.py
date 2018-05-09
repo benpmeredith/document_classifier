94913835a7696bf41e6c7b4f84920f2475fa27da
Ken Lo <thlo@uw.edu>
2018-05-09 15:32:39 -0700

    bug fixes

--- vector_distance_classifier.py
+++ vector_distance_classifier.py
@@ -2,9 +2,7 @@ import numpy as np
 from scipy.spatial import distance
 from sklearn.feature_extraction.text import TfidfVectorizer
 #import sklearn.feature_extraction.text
-class VDC:
-
-    
+class VDC():
     def __init__(self, class_threshold=0.25, use_idf=True, binary=True, max_features=100):
         self.Vectorizer = TfidfVectorizer(use_idf=use_idf, binary=binary, max_features=max_features)
         self.class_threshold = class_threshold
@@ -12,13 +10,13 @@ class VDC:
 
 
     def fit(self, train_text: list):
-        self.model_vector = np.mean(self.Vectorizer.fit_transform(train_text))
+        self.model_vector = np.mean(self.Vectorizer.fit_transform(train_text), axis=0)
     
 
     def predict(self, test_texts: list, in_binary=True):
         results = []
         for tt in test_texts:
-            test_vector = self.Vectorizer.transform(tt)
+            test_vector = self.Vectorizer.transform([tt])
             dist = np.mean(distance.cdist(self.model_vector, test_vector.toarray(), 'euclidean'))
             if in_binary:
                 if dist <= self.class_threshold:
